f09a04c347cefa74319610da1e79bcbca8cdd439
HassanShahzad <h.shahzad1996@gmail.com>
2026-03-08 14:25:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const images = ["images/fluid1.jpg","images/fluid2.jpg","images/fluid3.jpg"];
+let i = 0;
+
+function changeBackground(){
+  const hero = document.getElementById("hero");
+  if(hero) {
+    hero.style.backgroundImage = "url('" + images[i] + "')";
+    i++;
+    if(i >= images.length){ i=0; }
+  }
+}
+
+setInterval(changeBackground, 3000);
+changeBackground();
