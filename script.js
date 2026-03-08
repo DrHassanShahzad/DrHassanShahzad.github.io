2f2b207c26bdba01bf9c459d1b40ddb02098d61e
HassanShahzad <h.shahzad1996@gmail.com>
2026-03-08 14:51:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const images = ["images/fluid1.jpg","images/fluid2.png","images/fluid3.jpg"];
+const images = ["images/fluid1.jpg","images/fluid2.png","images/fluid3.png"];
 let i = 0;
 
 function changeBackground(){
@@ -13,3 +13,4 @@ function changeBackground(){
 setInterval(changeBackground, 3000);
 
 changeBackground();
+
